e93111d7c11e36acb2b4c63c3c83053f9264dd1f
kingsyung <kakpanyung@gmail.com>
2022-12-08 19:43:45 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,30 @@
 #include <stdio.h> 
-#innclude "recip.hpp"  
+  
  /** 
-  *   * main - Entry point 
-  *    * 
-  *     * Return: Always 0 (Success) 
-  *      */ 
+  *  * main - Entry point of code 
+  *   * 
+  *    * Description: prints the sizes in bytes of various "types" on the computer 
+  *     * it runs on 
+  *      * 
+  *       * Return: This returns an int 0 for success 
+  *        */ 
  int main(void) 
 	 { 
-		          int a; 
-		           long int b; 
-		            long long int c; 
-		             char d; 
-		              float f; 
+		          char a; 
+		           int c; 
+		            long int e; 
+		             long long int g; 
+		              float b; 
 		        
-		               printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d)); 
-		                printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a)); 
-			         printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b)); 
-			          printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c)); 
-			           printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)); 
+		               printf("Size of a char: %lu byte(s)\n", 
+				                                 (unsigned long)sizeof(a)); 
+		                printf("Size of an int: %lu byte(s)\n", 
+					                          (unsigned long)sizeof(c)); 
+			         printf("Size of a long int: %lu byte(s)\n", 
+					                           (unsigned long)sizeof(e)); 
+			          printf("Size of a long long int: %lu byte(s)\n", 
+					                            (unsigned long)sizeof(g)); 
+			           printf("Size of a float: %lu byte(s)\n", 
+					                             (unsigned long)sizeof(b)); 
 			            return (0); 
 			     }
