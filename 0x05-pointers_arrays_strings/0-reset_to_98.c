48c31cddb8d7f2613d88294fc5f38071fca30dba
kingsyung <kakpanyung@gmail.com>
2022-12-21 02:58:35 -0800

    rests

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * reset_to_98 - resets value of pointer to 98
  * @n: pointer to reset to 98
  *
- * Return: void
+ * Return: always 0
  */
 
 void reset_to_98(int *n)
+
 {
+
 	*n = 98;
+
 }
