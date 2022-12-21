8301d7f9e4e5450c5646f8d6381d9edaba3b4d06
kingsyung <kakpanyung@gmail.com>
2022-12-21 04:28:17 -0800

    committing

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,14 +5,12 @@
  * @s: input string.
  * Return: length of a string.
  */
-
 int _strlen(char *s)
-
 {
 	int count = 0;
 
 	while (*(s + count) != '\0')
 
-	count++;
+		count++;
 	return (count);
 }
