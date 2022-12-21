bf1eec30f551aa1ddb0c59dd27cf9c74e7ef6538
kingsyung <kakpanyung@gmail.com>
2022-12-21 05:39:44 -0800

    6-puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - printting one char out of 2 of a string.
+ * @str: input string.
+ * Return: always 0.
+ *
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
