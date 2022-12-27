782e09044ed1e2a0e61457574a250229d38d51f3
kingsyung <kakpanyung@gmail.com>
2022-12-27 05:37:22 -0800

    print_lower

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,14 +5,13 @@
  * Return: always 0
  */
 int main(void)
-
 {
-	char c = 'a';
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	while (c <= 'z')
+	for (i = 0; i < 26; i++)
 	{
-		putchar(c);
-		c++;
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
