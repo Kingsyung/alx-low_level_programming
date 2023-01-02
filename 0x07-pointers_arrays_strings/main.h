7f5ff2c7cede310bca82071b99c95bbe032c2217
kingsyung <kakpanyung@gmail.com>
2023-01-02 11:54:48 -0800

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
 #ifndef MAINN_H
 #define MAIN_H
-
 #include <stdio.h>
-#include <conio.h>
+/* #include <conio.h>*/
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
