48d1de7c54ecd07fedf9a8606e19a81bdc9683b4
kingsyung <kakpanyung@gmail.com>
2023-01-03 20:16:55 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-set_string.c -o 100-set_string

---
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	har *s0 = "Bob Dylan";
+	har *s1 = "Robert Allen";
+
+	printf("%s, %s\n", s0, s1);
+	set_string(&s1, s0);
+	printf("%s, %s\n", s0, s1);
+	return (0);
+}
