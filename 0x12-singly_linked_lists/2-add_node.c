ecff74f589e88f0ce43f6600c209a0ab4038d0be
kingsyung <kakpanyung@gmail.com>
2023-01-26 19:02:17 -0800

    2 add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,72 +1,28 @@
-Toggle navigationMenu 
-H
-holbertonschool-low_level_programming
-Project information
-Repository
-Files
-Commits
-Branches
-Tags
-Contributors
-Graph
-Compare
-Issues
-1
-Merge requests
-0
-CI/CD
-Deployments
-Packages and registries
-Monitor
-Analytics
-Wiki
-Snippets
-
-Close sidebar
-Open sidebar
-Martin Smith
-holbertonschool-low_level_programming
-Repository
-holbertonschool-low_level_programming
-0x12-more_singly_linked_lists
-2-add_nodeint.c
-2-add_nodeint.c
-763 bytes
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
-static listint_t *new_node(const int n);
+
 /**
- * add_nodeint - add a node at the head of a list.
- * @head : pointer of a pointer to a list.
- * @n : value to set the new list to.
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  *
- * Return: pointer to a pointer of listint_t
+ * Return: the address of the new element, or NULL if it fails
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+list_t *add_node(list_t **head, const char *str)
 {
-	listint_t *new;
-	if (!(*head))
-	{
-		*head = new_node(n);
-		return (*head);
-	}
-	new = new_node(n);
-	new->next = *head;
-	*head = new;
-	return (*head);
-}
-/**
- * new_node - creates a new listint_t node.
- * @n: number to place inside the node.
- *
- * Return: pointer to the new node.
- */
-static listint_t *new_node(const int n)
-{
-	listint_t *new;
-	new = malloc(sizeof(listint_t));
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
-	new->n = n;
-	new->next = NULL;
-	return (new);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+	return (*head);
 }
