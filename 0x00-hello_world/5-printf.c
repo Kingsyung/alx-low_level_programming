b431029a0f53d17935657de6444f404732dd5a0e
kingsyung <kakpanyung@gmail.com>
2024-01-19 12:11:51 -0800

    committing 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,16 @@
-#include <stdio.h> 
-  
- /** 
-  *   * main - starting point 
-  *    * 
-  *     * Return: Always 0 (Success) 
-  *      */ 
- int main(void) 
-	 { 
-		          printf("with proper grammar, but the outcome is a piece of art,\n"); 
-		           return (0); 
-		    }
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: To print "with proper grammar, but the o utcome is a piece
+* of art"
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but ");
+	printf("the outcome is a piece of art,\n");
+	return (0);
+}
