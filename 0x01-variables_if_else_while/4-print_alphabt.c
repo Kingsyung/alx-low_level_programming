950cda339db83b7196296704f062366e23dca6dc
kingsyung <kakpanyung@gmail.com>
2024-02-10 04:16:55 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,17 +9,16 @@
 
 int main(void)
 {
-	char ch;
+	char ch = 'a';
 
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	while (ch <= 'z')
 	{
-		putchar(ch);
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
 
-	}
-
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-	{
-		putchar(ch);
+		ch++;
 	}
 	putchar('\n');
 	return (0);
